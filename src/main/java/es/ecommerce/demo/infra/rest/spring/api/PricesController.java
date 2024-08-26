226eb48774415af1dd37830c256a775efeb57ebb
alvarodepieri <alvarodepieri@iti.es>
2024-08-26 13:34:41 +0200

    feat: Added more methods for error handling. - Created control that returns a response if any parameter is missing. - Added a generic error class to return the response code and error message. - Refactored PricesController.java to use the new error response.

--- src/main/java/es/ecommerce/demo/infra/rest/spring/api/PricesController.java
+++ src/main/java/es/ecommerce/demo/infra/rest/spring/api/PricesController.java
@@ -3,12 +3,14 @@ package es.ecommerce.demo.infra.rest.spring.api;
 import es.ecommerce.demo.app.service.PriceService;
 import es.ecommerce.demo.app.service.exception.PriceNotFoundException;
 import es.ecommerce.demo.infra.rest.spring.dto.PriceDto;
+import es.ecommerce.demo.infra.rest.spring.exception.ErrorResponse;
 import es.ecommerce.demo.infra.rest.spring.mapper.PriceDtoMapper;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MissingServletRequestParameterException;
 import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDateTime;
@@ -28,14 +30,27 @@ public class PricesController {
     }
 
     @ExceptionHandler(PriceNotFoundException.class)
-    public ResponseEntity<String> handlePriceNotFound(PriceNotFoundException ex) {
-        return new ResponseEntity<>(ex.getMessage(), HttpStatus.NOT_FOUND);
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public ResponseEntity<ErrorResponse> handlePriceNotFound(PriceNotFoundException ex) {
+        return new ResponseEntity<>(new ErrorResponse(HttpStatus.NOT_FOUND.value(),
+                ex.getMessage()), HttpStatus.NOT_FOUND);
     }
 
-    @ExceptionHandler({ IllegalArgumentException.class })
+    @ExceptionHandler(IllegalArgumentException.class)
     @ResponseStatus(HttpStatus.BAD_REQUEST)
-    public ResponseEntity<String> handleInvalidArguments(IllegalArgumentException ex) {
-        return new ResponseEntity<>("Error:" + ex.getMessage(),
-                HttpStatus.BAD_REQUEST);
+    public ResponseEntity<ErrorResponse> handleInvalidArguments(IllegalArgumentException ex) {
+        String error = "Error:" + ex.getMessage();
+        ErrorResponse errorResponse = new ErrorResponse(HttpStatus.BAD_REQUEST.value(), error);
+        return new ResponseEntity<>(errorResponse, HttpStatus.BAD_REQUEST);
     }
+
+    @ExceptionHandler(MissingServletRequestParameterException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ResponseEntity<ErrorResponse> handleMissingParams(MissingServletRequestParameterException ex) {
+        String name = ex.getParameterName();
+        String error = String.format("Parameter '%s' is missing", name);
+        ErrorResponse errorResponse = new ErrorResponse(HttpStatus.BAD_REQUEST.value(), error);
+        return new ResponseEntity<>(errorResponse, HttpStatus.BAD_REQUEST);
+    }
+
 }
